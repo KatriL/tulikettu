95747fef47f17198645b9cebfbacb2145bb7eda4
Katri Laulajainen <katri.laulajainen@hotmail.com>
2018-05-09 09:49:19 +0000

    Remove lines

--- srv/salt/firefox/syspref.js
+++ srv/salt/firefox/syspref.js
@@ -1,5 +1,1 @@
 user_pref("browser.startup.homepage", "http://www.jokerit.com");
-user_pref("browser.search.defaultenginename", "DuckDuckGo");
-user.pref("browser.search.order.1", "DuckDuckGo");
-user.pref("browser.search.order.2", "Google");
-user.pref("browser.search.order.3", "Wikipedia (en)");
